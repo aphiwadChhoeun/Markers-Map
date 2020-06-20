9297a7a3bc9677dbe4e093b37519bdc04d1bddc3
Aphiwad <aphiwad.chhoeun@cbsinteractive.com>
2020-06-19 18:17:43 -0700

    display table data on uploaded

--- src/components/ImportComponent.vue
+++ src/components/ImportComponent.vue
@@ -19,17 +19,66 @@
         </b-field>
       </div>
     </div>
+
+    <table-data :data="data" :columns="fields" />
   </div>
 </template>
 
 <script>
+import Papa from "papaparse";
+import TableData from "./TableData";
+
 export default {
   name: "ImportComponent",
+
+  components: {
+    TableData,
+  },
 
   data() {
     return {
       file: null,
+      data: [],
+      fields: [],
     };
+  },
+
+  watch: {
+    file(f) {
+      Papa.parse(f, {
+        header: true,
+        complete: (results) => {
+          let fields = [];
+          let data = [];
+
+          results.meta.fields.forEach((element) => {
+            fields.push({
+              field: element.toLowerCase(),
+              label: element,
+            });
+          });
+
+          results.data.forEach((element) => {
+            let t = {};
+            let keys = Object.keys(element);
+            let values = Object.values(element);
+
+            for (let i = 0; i < values.length; i++) {
+              if (values[i].length > 0) {
+                t[keys[i].toLowerCase()] = values[i];
+              }
+            }
+
+            if (Object.keys(t).length > 0) {
+              data.push(t);
+            }
+          });
+
+          this.fields = fields;
+          this.data = data;
+        },
+      });
+    },
   },
 };
 </script>
---
+++ src/components/TableData.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-table
+      :data="data"
+      :columns="columns"
+      :paginated="data.length > 5"
+      :per-page="5"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableData",
+
+  props: {
+    data: {
+      type: Array,
+    },
+    columns: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style></style>
